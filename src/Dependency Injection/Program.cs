37464258088f9b45323c07429cda51eb343946cb
Sojib <mdsojibhosen444@gmail.com>
2022-08-28 15:36:57 +0600

    ommit

--- src/Dependency Injection/Program.cs
+++ src/Dependency Injection/Program.cs
@@ -8,3 +8,10 @@ IButton button1 = new Engine();
 IButton button = new Break();
 Car car = new Car(button,button1);
 car.print();
+
+Propeerty_InjectionCar propertyCar=new Propeerty_InjectionCar();
+propertyCar.MyProperty = new Break();
+propertyCar.getProperty();
+
+Method__Injection_Car method_ = new Method__Injection_Car();
+method_.Add(button1);
